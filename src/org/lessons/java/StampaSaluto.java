0067d488f805b31b74af774b0b2917ed45a66bcb
buonoalex <loopatthecity@gmail.com>
2023-11-28 15:16:23 +0100

    Start template

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -2,6 +2,7 @@ package org.lessons.java;
 
 public class StampaSaluto {
     public static void main(String[] args) {
+        System.out.println("Come è bello essere qui con Generation");
 
     }
 }
